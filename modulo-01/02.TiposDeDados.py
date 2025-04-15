d63f9b10d5d18dd0a3c24aaca7b871b61b2cc249
Ester Miranda Cobos <ester.miranda.cobos@gmail.com>
2025-04-15 14:43:05 -0300

    maneira correta de formular os tipos de dados em Python

---
+++ modulo-01/02.TiposDeDados.py
@@ -0,0 +1,22 @@
+# string: sempre terá aspas, pode ser aspas duplas ou simples.
+
+print("Isso é uma string")
+print('Isso aqui também')
+
+# int – número inteiro. Tipo 0, 1, -5, 2025… Nesse caso, não é necessário inserir aspas duplas ou simples.
+
+print(10)
+print(-200)
+
+print("10" + "5")     # concatenação de strings
+print(10 + 5)         # soma de números
+
+# float – número com ponto (decimal). Em Python, usa-se ponto, não vírgula.
+
+print(3.14)
+print(-0.5)
+
+#  bool – valor booleano (verdadeiro/falso). Perfeito pra decisões e comparações. Sempre escrever a primeira letra em maiúscula.
+
+print(True)
+print(False)
