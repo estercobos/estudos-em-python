e0f3b14a6583655c0e7f21934ef3aabefadf337b
Ester Miranda Cobos <161371360+zorahmoon@users.noreply.github.com>
2025-04-17 15:34:47 -0300

    Delete modulo-01/14.Funcoes.py

--- modulo-01/14.Funcoes.py
+++
@@ -1,7 +0,0 @@
-# É um bloco de código reutilizável que executa uma tarefa específica. Você define uma vez e chama quantas quiser.Imagina uma função como uma máquina de suco: você coloca frutas (parâmetros), ela processa (executa comandos) e te entrega suco (retorno).
-
-def saudacao(nome):
-    return f"Olá, {nome}! Bem-vinda(o) ao mundo as funções!"
-
-mensagem = saudacao("Ester")
-print(mensagem)
