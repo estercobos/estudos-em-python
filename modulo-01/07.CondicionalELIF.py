b0795d37adaa928120b4bb06ca4d4a0f5cd1025c
Ester Miranda Cobos <161371360+zorahmoon@users.noreply.github.com>
2025-04-22 14:40:31 -0300

    Delete modulo-01 directory

--- modulo-01/07.CondicionalELIF.py
+++
@@ -1,8 +0,0 @@
-idade = int(input("Quantos anos você tem? "))
-
-if idade <= 10:
-    print("Você é uma criança!")
-elif idade >= 10 and idade < 18:
-    print("Você é um adolescente!")
-else:
-    print("Você é um adulto!")
